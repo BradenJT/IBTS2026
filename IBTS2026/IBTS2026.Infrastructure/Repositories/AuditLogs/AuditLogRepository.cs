d1e95b30e2b25c66d83b8f33d631180c9b4b36c7
Braden Townsell <bradenjparrish@gmail.com>
2026-01-20 20:36:22 -0600

    changes to repo pattern

--- IBTS2026/IBTS2026.Infrastructure/Repositories/AuditLogs/AuditLogRepository.cs
+++ IBTS2026/IBTS2026.Infrastructure/Repositories/AuditLogs/AuditLogRepository.cs
@@ -5,20 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IBTS2026.Infrastructure.Repositories.AuditLogs
 {
-    internal sealed class AuditLogRepository : Repository<AuditLog>, IAuditLogRepository
+    internal sealed class AuditLogRepository : RepositoryBase<AuditLog>, IAuditLogRepository
     {
         public AuditLogRepository(IBTS2026Context context)
             : base(context.AuditLogs)
         {
         }
-        public void Add(AuditLog auditLog)
-        {
-            DbSet.Add(auditLog);
-        }
+
+        public void Add(AuditLog auditLog) => AddEntity(auditLog);
 
         public Task<AuditLog?> GetByIdAsync(int id, CancellationToken ct)
-        {
-            return DbSet.FirstOrDefaultAsync(al => al.AuditLogId == id, ct);
-        }
+            => Query().FirstOrDefaultAsync(a => a.AuditLogId == id, ct);
     }
 }
